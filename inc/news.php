7f53d89317b81c43b34d75d4f8659ce6ffadea07
hello-world-ttj <tijotjoseph003@gmail.com>
2023-08-05 14:29:41 +0530

    #news

---
+++ inc/news.php
@@ -0,0 +1,67 @@
+<section class="blog section-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-5 mb-30">
+                    <h2 class="section-title">Latest News</h2>
+                    <hr class="border-2">
+                    <p class="section-title2">Quisque sed tellus nullam biben the volutpat dignissim pretium.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 owl-carousel owl-theme">
+                    <div class="news">
+                        <figure><img src="img/blog/4.jpg" alt="" class="img-fluid"></figure>
+                        <div class="caption">
+                            <h6>Resources</h6>
+                            <h4><a href="post.html">Cosina announces its fastest full-frame lens, the Nokton 50mm</a></h4>
+                            <p>Photographers viverra tristique duis vitae diam the neque nivamus aestan ateuene artines duru setlie suscipe the fermen.</p>
+                            <hr class="border-2">
+                            <div class="info-wrapper">
+                                <div class="more"><a href="post.html" class="link-btn" tabindex="0">Read more</a></div>
+                                <div class="date">20 December 2023</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="news left">
+                        <figure><img src="img/blog/3.jpg" alt="" class="img-fluid"></figure>
+                        <div class="caption">
+                            <h6>Lens News</h6>
+                            <h4>Which Camera Will Survive? Sony a1, Nikon Z9 and Canon R3</h4>
+                            <p>Photographers viverra tristique duis vitae diam the neque nivamus aestan ateuene artines duru setlie suscipe the fermen.</p>
+                            <hr class="border-2">
+                            <div class="info-wrapper">
+                                <div class="more"><a href="post.html" class="link-btn" tabindex="0">Read more</a></div>
+                                <div class="date">18 December 2023</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="news">
+                        <figure><img src="img/blog/2.jpg" alt="" class="img-fluid"></figure>
+                        <div class="caption">
+                            <h6>Mobile</h6>
+                            <h4><a href="post.html">The camera components inside the iPhone 11 Pro Max</a></h4>
+                            <p>Photographers viverra tristique duis vitae diam the neque nivamus aestan ateuene artines duru setlie suscipe the fermen.</p>
+                            <hr class="border-2">
+                            <div class="info-wrapper">
+                                <div class="more"><a href="post.html" class="link-btn" tabindex="0">Read more</a></div>
+                                <div class="date">16 December 2023</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="news left">
+                        <figure><img src="img/blog/1.jpg" alt="" class="img-fluid"></figure>
+                        <div class="caption">
+                            <h6>Resources</h6>
+                            <h4><a href="post.html">Fujifilm's new Instax Mini Evo Hybrid is an instant camera</a></h4>
+                            <p>Photographers viverra tristique duis vitae diam the neque nivamus aestan ateuene artines duru setlie suscipe the fermen.</p>
+                            <hr class="border-2">
+                            <div class="info-wrapper">
+                                <div class="more"><a href="post.html" class="link-btn" tabindex="0">Read more</a></div>
+                                <div class="date">14 December 2023</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
