66e5a21a0f34c23c544fdae1e9ed7577e0edbb72
hello-world-ttj <tijotjoseph003@gmail.com>
2023-08-05 14:34:13 +0530

    #client

--- index.php
+++ index.php
@@ -37,35 +37,9 @@
     <?php include './inc/testimonial.php' ?>
 
     <!-- Clients -->
-    <section class="clients">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-7">
-                    <div class="owl-carousel owl-theme">
-                        <div class="clients-logo">
-                            <a href="#0"><img src="img/clients/1.png" alt=""></a>
-                        </div>
-                        <div class="clients-logo">
-                            <a href="#0"><img src="img/clients/2.png" alt=""></a>
-                        </div>
-                        <div class="clients-logo">
-                            <a href="#0"><img src="img/clients/3.png" alt=""></a>
-                        </div>
-                        <div class="clients-logo">
-                            <a href="#0"><img src="img/clients/4.png" alt=""></a>
-                        </div>
-                        <div class="clients-logo">
-                            <a href="#0"><img src="img/clients/5.png" alt=""></a>
-                        </div>
-                        <div class="clients-logo">
-                            <a href="#0"><img src="img/clients/6.png" alt=""></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-
+    <?php include './inc/client.php' ?>
+    
+    <!-- Footer -->
     <?php include './inc/footer.php' ?>
     <!-- jQuery -->
     <script src="js/jquery-3.6.3.min.js"></script>
