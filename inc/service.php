e552f962fb8186dfd593b6420713229117dafaea
hello-world-ttj <tijotjoseph003@gmail.com>
2023-08-05 13:18:58 +0530

    #service

---
+++ inc/service.php
@@ -0,0 +1,63 @@
+<section class="services section-padding bg-blck">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 offset-md-3 mb-30 text-center">
+                    <h2 class="section-title">My Services</h2>
+                    <p class="section-title2">Quverra tristique justo duis vitae diam neque nivamus aestan ateuene artinaelition finibus viverra nec lacus setlie suscipe tristique.</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 border-01">
+                    <div class="item"> 
+                        <a href="services-page.html">
+                            <img src="img/icons/icon-1.svg" alt="">
+                            <h5>Photography</h5>
+                            <p>Photography bibendum eros amet vacun the vulputate in the vitae miss.</p>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-4 border-02">
+                    <div class="item"> 
+                        <a href="services-page.html">
+                        <img src="img/icons/icon-2.svg" alt="">
+                        <h5>Videography</h5>
+                        <p>Videography bibendum eros amen vacun the vulputate in the vitae miss.</p>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-4 border-03">
+                    <div class="item"> 
+                        <a href="services-page.html">
+                        <img src="img/icons/icon-3.svg" alt="">
+                        <h5>Photo Retouching</h5>
+                        <p>Photo Retouching bibenum eros amen vacun the vulputate the vitae miss.</p>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-4 border-04">
+                    <div class="item"> 
+                        <a href="services-page.html">
+                        <img src="img/icons/icon-4.svg" alt="">
+                        <h5>Aerial Photography</h5>
+                        <p>Aerial Photography bibendum eros amen vacun the vulputate in the miss.</p>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-4 border-05">
+                    <div class="item"> <img src="img/icons/icon-5.svg" alt="">
+                        <h5>Lightning Setup</h5>
+                        <p>Lightning Setup bibendum eros amen vacus duru in the vitae miss.</p>
+                    </div>
+                </div>
+                <div class="col-md-4 border-06">
+                    <div class="item"> 
+                        <a href="services-page.html">
+                        <img src="img/icons/icon-6.svg" alt="">
+                        <h5>Video Color Grading</h5>
+                        <p>Video Color Grading bibendum amen vacus the vulputate in the vitae.</p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
