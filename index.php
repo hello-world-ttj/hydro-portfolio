8b2f189a2655fb96815f68350383898564b86fc7
hello-world-ttj <tijotjoseph003@gmail.com>
2023-08-05 13:15:54 +0530

    #slider

--- index.php
+++ index.php
@@ -18,41 +18,9 @@
     <!-- Navbar -->
     <?php include 'inc/navbar.php' ?>
 
-
     <!-- Slider -->
-    <header id="slider-area" class="header slider-fade">
-        <div class="ruby-container">
-            <div class="owl-carousel owl-theme">
-                <!-- The opacity on the image is made with "data-overlay-dark="number". You can change it using the numbers 0-9. -->
-                <div class="text-left item bg-img" data-overlay-dark="2" data-background="img/slider/1.jpg">
-                    <div class="v-middle caption">
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <h4>Welcome to my</h4>
-                                    <h1>Hydro photography</h1>
-                                    <p>I love to pause the wild, happy and real moments of life, just to hear their stories untold.</p> <a href="#" class="button-primary">My works</a> <a href="#" class="button-tersiyer">Contact me</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="text-left item bg-img" data-overlay-dark="2" data-background="img/slider/2.jpg">
-                    <div class="v-middle caption">
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <h4>Hello there !</h4>
-                                    <h1>I'm Muhammed Rizwan</h1>
-                                    <p>I am professional photographer based on New York, creating dreamscapes with black, white and shades in-between.</p> <a href="#" class="button-primary">My works</a> <a href="#" class="button-tersiyer">Contact me</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </header>
+    <?php include './inc/slider.php' ?>
+    
     <!-- About section -->
     <section class="about section-padding">
         <div class="container">
