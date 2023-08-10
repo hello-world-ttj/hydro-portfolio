a01fb9ea4e57f542b376dbc5cb56df1ac49cd755
hello-world-ttj <tijotjoseph003@gmail.com>
2023-08-10 15:04:20 +0530

    keywords, description

--- inc/head.php
+++ inc/head.php
@@ -3,6 +3,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <title>HYDRO PHOTOGRAHY</title>
+    <meta name="description" content="Welcome to the official website of Muhammed Rizwan, a fashion photographer with over five years of experience in the photography industry">
+    <meta name="keywords" content="Muhammed Rizwan, fashion photographer, hydro photography">
     <link rel="shortcut icon" href="img/favicon.ico" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,300;0,400;0,500;0,600;0,700;1,400&amp;display=swap">
     <link rel="stylesheet" href="css/plugins.css" />
